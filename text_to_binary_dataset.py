46923e7aff7fd0624ceebc5809292c8c547c165c
Alex Bikeyev <alex.bikeyev@protonmail.com>
2025-04-30 05:48:54 +1200

    update author attribution to design credit in multiple files

--- text_to_binary_dataset.py
+++ text_to_binary_dataset.py
@@ -1,6 +1,6 @@
 # Code for Paper: "Polymorphic Graph Classifier"
 # http://dx.doi.org/10.13140/RG.2.2.15744.55041
-# Author: Alexander Bikeyev
+# Design: Alexander Bikeyev
 # Date: 2025-04-20
 # LICENSE: AGPL v3
 
--- visualize_brain_stats.py
+++ visualize_brain_stats.py
@@ -2,7 +2,7 @@
 
 # Code for Paper: "Polymorphic Graph Classifier"
 # http://dx.doi.org/10.13140/RG.2.2.15744.55041
-# Author: Alexander Bikeyev
+# Design: Alexander Bikeyev
 # Date: 2025-04-20
 # LICENSE: AGPL v3
 
